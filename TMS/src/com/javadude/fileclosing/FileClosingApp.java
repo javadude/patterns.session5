b48d03c2b1b17146ed671785d5804d7d897e59de
Scott Stanchfield <scott@javadude.com>
2015-08-20 07:42:27 -0400

    Move close to finally

--- TMS/src/com/javadude/fileclosing/FileClosingApp.java
+++ TMS/src/com/javadude/fileclosing/FileClosingApp.java
@@ -7,16 +7,21 @@ import java.io.PrintWriter;
 public class FileClosingApp {
 	// NOTE: THIS IS BAD (and will be bad for a while...)
 	public static void main(String[] args) {
+		FileWriter fw = null;
+		PrintWriter pw = null;
 		try {
-			FileWriter fw = new FileWriter("foo.txt");
-			PrintWriter pw = new PrintWriter(fw);
+			fw = new FileWriter("foo.txt");
+			pw = new PrintWriter(fw);
 			pw.println("Hello");
 			pw.println("Goodbye");
-			pw.close();
 			
 		} catch (IOException e) {
 			// DO SOMETHING MORE HERE IN REAL APPS...
 			e.printStackTrace();
+
+		} finally {
+			if (pw != null)
+				pw.close();
 		}
 	}
 }
